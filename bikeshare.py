669622a6a3a60fd1388f227e70690f7fd541f6ce
Stheven-Sutanto <stheven-s@hotmail.com>
2022-01-03 22:50:14 +1100

    Refactoring: updating code description
    updating month and day filter options
    updating description user age by year of birth

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-import calendar 
+import calendar
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
@@ -13,8 +13,8 @@ def get_filters():
 
     Returns:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) month - name of the month to filter by, or "all" to apply all months
+        (str) day - name of the day of week to filter by, or "all" to apply all days of week
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
@@ -24,18 +24,18 @@ def get_filters():
         if city in cities:
             break
         else:
-            print("\n Invalid city name. Please choose a valid city name") 
+            print("\n Invalid city name. Please choose a valid city name")
 
 
     # TO DO: get user input for month (all, january, february, ... , june)
     while True:
-        months= ['january','february','march','april','may','june','all'] 
+        months= ['january','february','march','april','may','june','all']
         month= input('\nPlease select the month between January to June or type all if you want to select all months \n').lower()
         if month in months:
             break
         else:
-            print("\n Invalid month selected. Please select the correct month or type all if you want select all months") 
-            
+            print("\n Invalid month selected. Please select the correct month or type all if you want select all months")
+
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
     while True:
          days=['monday','tuesday','wednesday','thursday','friday','saturday','sunday','all']
@@ -43,7 +43,7 @@ def get_filters():
          if day in days:
             break
          else:
-            print("\n Invalid day of week selected. Please select the correct day or type all if you want select all days of week") 
+            print("\n Invalid day of week selected. Please select the correct day or type all if you want select all days of week")
 
 
     print('-'*40)
@@ -63,14 +63,14 @@ def load_data(city, month, day):
     """
     #Load city data file into data frame
     df = pd.read_csv(CITY_DATA[city])
-    
+
      # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 
     # extract month and day of week from Start Time to create new columns
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-    
+
     # filter by month if applicable
     if month != 'all':
         # use the index of the months list to get the corresponding int
@@ -96,14 +96,14 @@ def time_stats(df):
 
     # TO DO: display the most common month
     common_month = df['month'].mode()[0]
-    print('The most common month is',calendar.month_name[common_month]) 
+    print('The most common month is',calendar.month_name[common_month])
 
     # TO DO: display the most common day of week
     common_dow = df['day_of_week'].mode()[0]
     print('The most common day of week is', common_dow)
 
     # TO DO: display the most common start hour
-    df['Start Hour'] = df['Start Time'].dt.hour #extract hour from the start time column 
+    df['Start Hour'] = df['Start Time'].dt.hour #extract hour from the start time column
     common_sh = df['Start Hour'].mode()[0]
     print('The most common start hour is', common_sh)
 
@@ -174,16 +174,16 @@ def user_stats(df,city):
         gender = df['Gender'].value_counts()
         print('\nUser by gender are\n',gender)
 
-    # TO DO: Display earliest, most recent, and most common year of birth
+    # TO DO: Display oldest, youngest, and most common year of birth
         oldest = int(df['Birth Year'].min())
         print('\nThe oldest year of birth is', oldest)
-        
+
         youngest = int(df['Birth Year'].max())
         print('The youngest year of birth is', youngest)
-        
+
         most_common = df['Birth Year'].mode()[0]
         print('Most users are born in', most_common)
-           
+
     else:
         print('\n User gender and year of birth are not available')
 
@@ -193,15 +193,15 @@ def user_stats(df,city):
 
 def display_data(df):
     a=0
-    answer = input('Please type "Yes" if you would like to view the first 5 lists of data\n').lower()
+    answer = input('Please type "Yes" if you would like to view the first 5 list of data\n').lower()
     while True:
         if answer == 'no':
             break
         print(df[a:a+5])
-        answer = input('Please type "Yes"if you would like to view the next 5 entries of data\n').lower()
+        answer = input('Please type "Yes" if you would like to view the next 5 entries of data\n').lower()
         a=+5
-                
-        
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -213,7 +213,7 @@ def main():
         user_stats(df,city)
         display_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart ? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
